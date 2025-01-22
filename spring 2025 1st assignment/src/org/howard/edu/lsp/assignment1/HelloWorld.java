a3056774f4f085efd17f36ae0a0391309766b080
ryaskye <ryaskye@gmail.com>
2025-01-22 17:25:28 -0500

    second time

--- spring 2025 1st assignment/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ spring 2025 1st assignment/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,13 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+	/**
+	 * Rya McKinnon
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
 
 	}
 
